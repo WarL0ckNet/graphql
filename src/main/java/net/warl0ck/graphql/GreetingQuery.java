3b323e5897a46041ae8132697ba148927658f068
WarL0ck <main@warl0ck.net>
2018-05-07 21:35:52 +0700

    Initial commit

---
+++ src/main/java/net/warl0ck/graphql/GreetingQuery.java
@@ -0,0 +1,16 @@
+package net.warl0ck.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingQuery implements GraphQLQueryResolver {
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public Greeting getGreeting(String id) {
+        return greetingRepository.find(id);
+    }
+}
---
+++ src/main/java/net/warl0ck/graphql/SpringBootGraphQL.java
@@ -0,0 +1,12 @@
+package net.warl0ck.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQL {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphQL.class, args);
+    }
+}
